478e350b8640f466cf75318a04b189a8665fc871
Stuart Jennings <stuartjennings13@gmail.com>
2017-08-08 22:07:01 +0100

    extend functional test with basic homepage functionality to add a node

--- collator/core/tests/test_core_functional_tests.py
+++ collator/core/tests/test_core_functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,12 +14,28 @@ class NewVisitorTest(unittest.TestCase):
     # Unfinished (also is this in the right place?)
     def test_can_create_node_and_retrieve_it_later(self):
 
+        # Homepage is loaded
         self.browser.get('http://localhost:8000')
+
         self.assertIn('Collator', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Collator', header_text)
         body_text = self.browser.find_element_by_tag_name('h4').text
         self.assertIn('Organise', body_text)
+
+        # Add node interface is present
+        inputbox = self.browser.find_element_by_id('id_new_node')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Add a new node...')
+
+        # Can add a new node and it appears on the list on the homepage
+        inputbox.send_keys('Museums')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        nodes = self.browser.find_elements_by_class_name('homepage_node')
+        self.assertTrue(
+            any(node.text == 'Museums' for node in nodes)
+        )
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
